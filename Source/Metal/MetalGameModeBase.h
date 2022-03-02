98355d23c0d0f5d0e475cea7242efdb0dafeff1a
Yordy Leonidas Moreno Vásquez <yordy.lmv.2000@gmail.com>
2022-03-01 23:12:36 -0500

    init

---
+++ Source/Metal/MetalGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MetalGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class METAL_API AMetalGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
